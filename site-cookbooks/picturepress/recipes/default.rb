e7506e06a2d737d8cc78c58ae793df130836779d
Hao Wei <imwillmouse@gmail.com>
2015-02-04 14:34:10 +0800

    add run_list

--- site-cookbooks/picturepress/recipes/default.rb
+++ site-cookbooks/picturepress/recipes/default.rb
@@ -1,8 +1,12 @@
-#
-# Cookbook Name:: picturepress
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+package "redis-server"
+package "nodejs"
+package "imagemagick"
+
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+
+rbenv_ruby "2.1.5"
+
+rbenv_gem "bundler" do
+  ruby_version "2.1.5"
+end
