e99dea9b97a6d00a91f968fa581615f63a8691f9
Hao Wei <imwillmouse@gmail.com>
2015-02-05 17:08:29 +0800

    remove roles

--- roles/sudoer.rb
+++
@@ -1,9 +0,0 @@
-# default_attributes(
-#   "authorization" => {
-#     "sudo" => {
-#       "groups" => ["admin", "wheel", "sysadmin"],
-#       "users" => ["depolyer", "vagrant"],
-#       "passwordless" => true
-#     }
-#   }
-# )
